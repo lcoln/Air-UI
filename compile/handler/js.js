b2dcbb1c01898dbe1b3dc7a0901f4c331e3bdcd6
coln <coln@tencent.com>
2022-03-29 20:17:37 +0800

    refactor: 💡 优化compile js.js判断

--- compile/handler/js.js
+++ compile/handler/js.js
@@ -33,20 +33,20 @@ function write (dest, code, filename, project, format) {
 module.exports = async function build ({source, dest, filename, format, project}) {
   const tmp = source.split('/')
   const l = tmp.length - tmp.lastIndexOf(project)
-  // console.log({l, source, tmp, project})
+  console.log({l, source, tmp, project})
 
-  if (/\/src\/config\.js/.test(source) || 
-    (l > 3 || (l === 3 && !/^index\.(t|j)s(x?)$/.test(tmp[tmp.length - 1]))) &&
-    project
-  ) {
-    return
-  }
-  // console.log(56789)
   // wc项目里的js无需进行rollup打包, 会直接压缩输出
   if (format === 'js' && (/\/wc\//.test(source) || /\/src\/index\.js/.test(source))) {
     // console.log({dest, source, filename})
     return write(dest, fs.cat(source), filename, project, format)
   }
+  if (/\/src\/config\.js/.test(source) || 
+    (l > 3 || (l === 3 && !/^index\.(t|j)s$/.test(tmp[tmp.length - 1]))) &&
+    project
+  ) {
+    return
+  }
+  console.log(56789)
   // console.log({filename})
 
   let configs = makeConfig.genConfigs({source, dest, filename, project, format})
